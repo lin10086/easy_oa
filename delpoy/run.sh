67a63173407fb036be8082ca5bab33cef5824483
linsuoting <fuwuzhou@meituan.com>
2019-12-26 15:53:33 +0800

    给远程上传pom.xml文件

--- delpoy/run.sh
+++ delpoy/run.sh
@@ -14,3 +14,40 @@
 # -----------------------------------
 
 
+#!/bin/bash
+echo =================================
+echo  interactive自动化部署脚本启动
+echo =================================
+
+echo 进入/data/interactive
+cd /usr/git
+cd /data/interactive
+
+echo 开始pull版本
+git pull
+
+echo 开始编译文件
+mvn clean package -Dmaven.test.skip=true
+
+echo 进入编译完成文件
+cd /data/interactive/interactive-web/target
+
+echo 文件重命名
+mv interactive-web.war ROOT.war
+
+echo 关闭tomcat服务器
+sh /data/software/tomcat/bin/shutdown.sh
+
+echo 删除以往文件
+rm -rf /data/software/tomcat/webapps/ROOT
+
+echo 移动文件
+mv ROOT.war /data/software/tomcat/webapps/
+
+echo 重启服务器
+sh /data/software/tomcat/bin/startup.sh
+
+作者：song先生
+链接：http://www.imooc.com/article/20056
+来源：慕课网
+本文原创发布于慕课网 ，转载请注明出处，谢谢合作
